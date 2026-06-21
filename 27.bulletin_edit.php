b608705109abeb231625c299204ef8bc119b9685
gfp340 <133774242+gfp340@users.noreply.github.com>
2026-06-21 20:30:01 +0800

    Create 27.bulletin_edit.php

---
+++ 27.bulletin_edit.php
@@ -0,0 +1,21 @@
+<?php
+
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{   
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+        if (!mysqli_query($conn, "update bulletin set title='{$_POST['title']}',content='{$_POST['content']}',time='{$_POST['time']}',type={$_POST['type']} where bid='{$_POST['bid']}'")){
+            echo "修改錯誤";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+		#將佈告進行更新至資料庫裡
+        }else{
+            echo "修改成功，三秒鐘後回到佈告欄列表";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+        }
+    }
+
+?>
